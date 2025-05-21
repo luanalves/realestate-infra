4edf2ae2355088f22798316f8b199f98d56df5e5
Luan alves <luan_alves@hotmail.com>
2025-05-21 06:09:39 -0300

    update in file clean, to cleaner containers and app

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 # docker compose down -v --remove-orphans       # derruba tudo e limpa volumes/containers órfãos
-docker compose down 
+docker compose down
 docker system prune -a --volumes -f           # limpa imagens, containers parados, volumes não usados, etc.
 docker compose build --no-cache               # reconstrói tudo do zero
 docker compose up -d                          # sobe tudo em background
